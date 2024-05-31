d57f42b8c3811e2cefab3577d50985ba0f5c189d
Adam Zielonka <root@adamzielonka.pro>
2024-05-31 16:14:25 +0200

    Migrate to vite

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import js from "@eslint/js";
+
+export default [
+  {
+    languageOptions: { 
+      globals: globals.browser 
+    }
+  },
+  js.configs.recommended,
+  {
+    rules: {
+      "indent": ["warn", 2, { "SwitchCase": 1 }],
+      "linebreak-style": ["warn", "unix"],
+      "quotes": ["warn", "single"],
+      "semi": ["warn", "never"],
+      "max-len": ["warn", { "code": 120 }],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "keyword-spacing": ["warn", { "before": true, "after": true }],
+    }
+  }
+];
