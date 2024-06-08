89fc54c626712d551db5a017d7a3d3c8a19aeed4
Adam Zielonka <root@adamzielonka.pro>
2024-06-08 08:54:21 +0200

    Replace reduce with for

--- src/utils.js
+++ src/utils.js
@@ -12,21 +12,23 @@ export const create2dArray = (width, height, fill) => (
 export const map2d = (arr2d, map) => arr2d.map((line, i) => line.map((cell, j) => map(cell, i, j)))
 
 export const getNextGeneration = (board) => map2d(board, (cell, i, j) => {
-  const liveCells = [
+  const aroundCells = [
     [i - 1, j - 1], [i, j + 1], [i + 1, j - 1],
     [i - 1, j    ]/*[i, j  ]*/, [i + 1, j    ],
     [i - 1, j + 1], [i, j - 1], [i + 1, j + 1],
-  ].reduce((counter, [x, y]) => {
+  ]
+
+  let livingCells = 0
+  for (let [x, y] of aroundCells) {
     if (x < 0) x = board.length - 1
     if (x >= board.length) x = 0
     if (y < 0) y = board[x].length - 1
     if (y >= board[x].length) y = 0
 
-    if (board[x][y] === false) return counter
-    return counter + 1
-  }, 0)
+    if (board[x][y]) livingCells++
+  }
 
-  if (cell && (liveCells < 2 || liveCells > 3)) return false
-  if (!cell && liveCells === 3) return true
+  if (cell && (livingCells < 2 || livingCells > 3)) return false
+  if (!cell && livingCells === 3) return true
   return cell
 })
